1751f8872fcf58cd8af62bd5c2487240798dbb55
ElJohanitoG <johanalejandrorosaleszambrano@gmail.com>
2024-09-01 12:49:45 -0400

    Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,45 @@
+# Welcome to the Interactive Python Tutorial.
+# Start by choosing a chapter and
+# write your code in this window
+
+#variables
+var1= 0
+var2= 0
+resultado = 0
+ed= "El resultado es:"
+
+def bienvenida():
+    print("Bienvenido a La Calculadora")
+    texto = """Escribe suma , resta , multi o div
+    para realizar operaciones """
+    print(texto)
+    op = input("Ingrese Una Operación")
+    
+def suma():
+    var1 = int(input("Ingrese un Número"))
+    var2 = int(input("Ingrese otro número"))
+    resultado = var1 + var2
+    print(ed)
+    print(resultado)
+    print(bienvenida)
+    
+def resta():
+    var1 = int(input("Ingrese un número"))
+    var2 = int(input("Ingrese otro número"))
+    resultado = var1 + var2
+    print(ed)
+    print(resultado)
+    print(bienvenida)
+          
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
